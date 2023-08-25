20866391ef04500dcff84be0dbd2f66eb19018aa
ViktorVanHulle <viktor.van.hulle@gmail.com>
2023-08-25 18:00:06 +0200

    Initial Commit

---
+++ IOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSAssignment
+//
+//  Created by Viktor van Hulle on 25/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
